c77c201459b32913b67089092d9829241b02116d
Lasse <lassetange@gmail.com>
2020-04-05 14:08:23 +0200

    Updates

---
+++ precache-manifest.329d1397a397e5033391b3c4437557d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ab8922fd708ec88c7e9c39999d5589d",
+    "url": "/react-collapse-simple/index.html"
+  },
+  {
+    "revision": "07cb5e161632b6d7e49b",
+    "url": "/react-collapse-simple/static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "ac0fb25d4fce06f34bbe",
+    "url": "/react-collapse-simple/static/js/2.5d20614e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-collapse-simple/static/js/2.5d20614e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07cb5e161632b6d7e49b",
+    "url": "/react-collapse-simple/static/js/main.15253f11.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "/react-collapse-simple/static/js/main.15253f11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2ac0f2cfcde649e58ce",
+    "url": "/react-collapse-simple/static/js/runtime-main.738af945.js"
+  }
+]);
